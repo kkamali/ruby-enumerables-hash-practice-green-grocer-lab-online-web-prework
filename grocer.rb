2d036e2167477cf6fd80f5710180828954bb04b1
Kathleen Kamali <kathleenkamali@gmail.com>
2019-09-16 20:19:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -31,9 +31,7 @@ def apply_clearance(cart)
   cart.each_pair{|key, value|
     if cart[key][:clearance]
       old_price = cart[key][:price]
-      puts old_price
       new_price = cart[key][:price] - (cart[key][:price] * 0.2).round(2)
-      puts new_price
       cart[key][:price] = new_price
     end
   }
